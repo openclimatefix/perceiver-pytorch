ec0a62829298ac9a818a30ed65e1d4f4c7853ee4
Jacob Bieker <jacob@openclimatefix.org>
2021-09-03 13:59:04 +0100

    Add imag encoder tests

--- tests/test_encoders.py
+++ tests/test_encoders.py
@@ -2,11 +2,58 @@ from perceiver_pytorch.encoders import ImageEncoder
 import torch
 import pytest
 
-@pytest.mark.parametrize("prep_type", ["conv", "patches", "conv1x1", "pixels"])
-def test_image_encoder(prep_type):
+
+@pytest.mark.parametrize("prep_type", ["conv", "conv1x1"])
+def test_conv_image_encoder(prep_type):
     encoder = ImageEncoder(prep_type=prep_type, output_channels=48)
     image = torch.randn(2, 12, 256, 256)
     with torch.no_grad():
         out = encoder(image)
-    print(out.shape)
+    assert not torch.isnan(out).any(), "Output included NaNs"
+    assert out.size() == (2, 48, 64, 64)
 
+
+def test_patches_image_encoder():
+    encoder = ImageEncoder(prep_type="patches", output_channels=48)
+    image = torch.randn(2, 12, 256, 256)
+    with torch.no_grad():
+        out = encoder(image)
+    assert not torch.isnan(out).any(), "Output included NaNs"
+    assert out.size() == (2, 192, 64, 64)
+
+
+def test_pixels_image_encoder():
+    encoder = ImageEncoder(prep_type="pixels", output_channels=48)
+    image = torch.randn(2, 12, 256, 256)
+    with torch.no_grad():
+        out = encoder(image)
+    assert not torch.isnan(out).any(), "Output included NaNs"
+    assert out.size() == (2, 12, 64, 64)
+
+
+@pytest.mark.parametrize("prep_type", ["conv", "conv1x1"])
+def test_conv_video_encoder(prep_type):
+    encoder = ImageEncoder(prep_type=prep_type, output_channels=48)
+    image = torch.randn(2, 6, 12, 256, 256)
+    with torch.no_grad():
+        out = encoder(image)
+    assert not torch.isnan(out).any(), "Output included NaNs"
+    assert out.size() == (2, 6, 48, 64, 64)
+
+
+def test_patches_video_encoder():
+    encoder = ImageEncoder(prep_type="patches", output_channels=48)
+    image = torch.randn(2, 6, 12, 256, 256)
+    with torch.no_grad():
+        out = encoder(image)
+    assert not torch.isnan(out).any(), "Output included NaNs"
+    assert out.size() == (2, 6, 192, 64, 64)
+
+
+def test_pixels_video_encoder():
+    encoder = ImageEncoder(prep_type="pixels", output_channels=48)
+    image = torch.randn(2, 6, 12, 256, 256)
+    with torch.no_grad():
+        out = encoder(image)
+    assert not torch.isnan(out).any(), "Output included NaNs"
+    assert out.size() == (2, 6, 12, 64, 64)
