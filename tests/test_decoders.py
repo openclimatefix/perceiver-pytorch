38b7620b5e6bb94d163ff9b56e4820e81f21d346
Jacob Bieker <jacob@openclimatefix.org>
2021-09-06 14:04:34 +0100

    Readd skipped test with issue #7

--- tests/test_decoders.py
+++ tests/test_decoders.py
@@ -61,13 +61,14 @@ def test_conv1x1_video_decoder():
     assert out.size() == (2, 3, 12, 16, 16)
 
 
-#def test_conv3d_video_decoder():
-#    decoder = ImageDecoder(postprocess_type="conv", output_channels=12, input_channels=48, spatial_upsample=4, temporal_upsample=2)
-#    inputs = torch.randn(2, 1, 48, 64, 64)
-#    with torch.no_grad():
-#        out = decoder(inputs)
-#    assert not torch.isnan(out).any(), "Output included NaNs"
-#    assert out.size() == (2, 2, 12, 256, 256)
+@pytest.mark.skip("This test does not work because the Conv3DUpsample decoder is off by a few timesteps Jacob Sept 2021 - see issue #7")
+def test_conv3d_video_decoder():
+    decoder = ImageDecoder(postprocess_type="conv", output_channels=12, input_channels=48, spatial_upsample=4, temporal_upsample=2)
+    inputs = torch.randn(2, 1, 48, 64, 64)
+    with torch.no_grad():
+        out = decoder(inputs)
+    assert not torch.isnan(out).any(), "Output included NaNs"
+    assert out.size() == (2, 2, 12, 256, 256)
 
 
 def test_patches_video_decoder():
