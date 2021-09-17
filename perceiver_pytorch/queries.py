f6ce7b4c5b8c4fe03dfa73c918dcccda27df3ce5
Jacob Bieker <jacob@openclimatefix.org>
2021-09-17 16:54:20 +0100

    Add test, conv layer encoding
    Keep the latent space as the 4D tensor and then linearize at the end, the same as what the Perceiver models do

--- perceiver_pytorch/queries.py
+++ perceiver_pytorch/queries.py
@@ -3,6 +3,7 @@ from torch.distributions import uniform
 from typing import List, Union, Tuple
 from math import prod
 from perceiver_pytorch.utils import encode_position
+import einops
 
 
 class LearnableQuery(torch.nn.Module):
@@ -14,6 +15,7 @@ class LearnableQuery(torch.nn.Module):
         self,
         query_dim: int,
         query_shape: Union[Tuple[int], List[int]],
+        conv_layer: str = "3d",
         max_frequency: float = 16.0,
         num_frequency_bands: int = 64,
         frequency_base: float = 2.0,
@@ -27,7 +29,7 @@ class LearnableQuery(torch.nn.Module):
             query_shape: The final shape of the query, generally, the (T, H, W) of the output
         """
         super().__init__()
-        self.query_shape = prod(query_shape)  # Flatten the shape
+        self.query_shape = query_shape
         # Need to get Fourier Features once and then just append to the output
         self.fourier_features = encode_position(
             1,
@@ -37,15 +39,45 @@ class LearnableQuery(torch.nn.Module):
             num_frequency_bands=num_frequency_bands,
             sine_only=sine_only,
         )
+        print(self.fourier_features.shape)
         self.query_dim = query_dim
-        self.learnable_query = torch.nn.Linear(self.query_dim, self.query_dim)
+        if conv_layer == "3d":
+            conv = torch.nn.Conv3d
+        elif conv_layer == "2d":
+            conv = torch.nn.Conv2d
+        else:
+            raise ValueError(f"Value for 'layer' is {layer} which is not one of '3d', '2d'")
+        self.conv_layer = conv_layer
+        self.layer = conv(in_channels=query_dim, out_channels=query_dim, kernel_size=3, padding=1)
+        # Linear layer to compress channels down to query_dim size?
+        self.fc = torch.nn.Linear(self.query_dim, self.query_dim)
         self.distribution = uniform.Uniform(low=torch.Tensor([0.0]), high=torch.Tensor([1.0]))
 
     def forward(self, x: torch.Tensor):
-        z = self.distribution.sample((x.shape[0], self.query_future_size, self.query_dim)).type_as(
+        print(f"Batch: {x.shape[0]} Query: {self.query_shape} Dim: {self.query_dim}")
+        z = self.distribution.sample((x.shape[0], self.query_dim, *self.query_shape)).type_as(
             x
-        )
+        )  # [B, Query, T, H, W, 1] or [B, Query, H, W, 1]
+        z = torch.squeeze(z, dim=-1)  # Extra 1 for some reason
+        print(f"Z: {z.shape}")
         # Do 3D or 2D CNN to keep same spatial size, concat, then linearize
-        queries = self.learnable_query(z)
-        # TODO Add Fourier Features
-        return queries
+        if self.conv_layer == "2d":
+            # Iterate through time dimension
+            outs = []
+            for i in range(x.shape[1]):
+                outs.append(self.layer(z[:, i, :, :, :]))
+            query = torch.stack(outs, dim=1)
+        else:
+            query = self.layer(z)
+        # Add Fourier Features
+        ff = einops.repeat(
+            self.fourier_features, "b ... -> (repeat b) ...", repeat=x.shape[0]
+        )  # Match batches
+        print(ff.shape)
+        query = torch.cat([query, ff], dim=-1)
+        print(query.shape)
+        # concat to channels of data and flatten axis
+        query = einops.rearrange(query, "b ... d -> b (...) d")
+        # Need query to end with query_dim channels
+
+        return query
---
+++ tests/test_queries.py
@@ -0,0 +1,20 @@
+import pytest
+import torch
+from perceiver_pytorch.queries import LearnableQuery
+
+
+@pytest.mark.parametrize("layer_shape", ["2d", "3d"])
+def test_learnable_query(layer_shape):
+    query_creator = LearnableQuery(
+        query_dim=32,
+        query_shape=(24, 128, 128),
+        conv_layer=layer_shape,
+        max_frequency=64.0,
+        frequency_base=2.0,
+        num_frequency_bands=128,
+        sine_only=False,
+    )
+    x = torch.randn((16, 24, 12, 128, 128))
+    out = query_creator(x)
+
+    pass
