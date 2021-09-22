a4fb689296f22db750a061a17a13050ba7453c88
Jacob Bieker <jacob@openclimatefix.org>
2021-09-22 11:28:59 +0100

    Add test to ensure query works with PerceiverIO

--- tests/test_queries.py
+++ tests/test_queries.py
@@ -1,6 +1,8 @@
 import pytest
 import torch
 from perceiver_pytorch.queries import LearnableQuery
+from perceiver_pytorch.perceiver_io import PerceiverIO
+import einops
 
 
 @pytest.mark.parametrize("layer_shape", ["2d", "3d"])
@@ -16,5 +18,36 @@ def test_learnable_query(layer_shape):
     )
     x = torch.randn((16, 24, 12, 128, 128))
     out = query_creator(x)
+    assert out.shape == (16, 393216, 803)
 
-    pass
+
+@pytest.mark.parametrize("layer_shape", ["2d", "3d"])
+def test_learnable_query_qpplication(layer_shape):
+    output_shape = (24, 128, 128)
+    query_creator = LearnableQuery(
+        channel_dim=32,
+        query_shape=output_shape,
+        conv_layer=layer_shape,
+        max_frequency=64.0,
+        frequency_base=2.0,
+        num_frequency_bands=128,
+        sine_only=False,
+    )
+    with torch.no_grad():
+        query_creator.eval()
+        x = torch.randn((4, 24, 12, 128, 128))
+        out = query_creator(x)
+
+        model = PerceiverIO(depth=6, dim=100, queries_dim=query_creator.output_shape()[-1])
+        model.eval()
+        model_input = torch.randn((4, 256, 100))
+        model_out = model(model_input, queries=out)
+        # Reshape back to correct shape
+        model_out = einops.rearrange(
+            model_out,
+            "b (t h w) c -> b t c h w",
+            t=output_shape[0],
+            h=output_shape[1],
+            w=output_shape[2],
+        )
+        assert model_out.shape == (4, 24, 803, 128, 128)
