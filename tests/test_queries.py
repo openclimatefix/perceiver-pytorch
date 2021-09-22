1188bae501c28eb203598100d83d300a9419fc90
Jacob Bieker <jacob@openclimatefix.org>
2021-09-22 11:38:21 +0100

    Reduce number of Fourier Features

--- tests/test_queries.py
+++ tests/test_queries.py
@@ -23,24 +23,24 @@ def test_learnable_query(layer_shape):
 
 @pytest.mark.parametrize("layer_shape", ["2d", "3d"])
 def test_learnable_query_qpplication(layer_shape):
-    output_shape = (6, 32, 32)
+    output_shape = (6, 16, 16)
     query_creator = LearnableQuery(
         channel_dim=32,
         query_shape=output_shape,
         conv_layer=layer_shape,
         max_frequency=64.0,
         frequency_base=2.0,
-        num_frequency_bands=128,
+        num_frequency_bands=32,
         sine_only=False,
     )
     with torch.no_grad():
         query_creator.eval()
-        x = torch.randn((4, 6, 12, 32, 32))
+        x = torch.randn((2, 6, 12, 16, 16))
         out = query_creator(x)
 
         model = PerceiverIO(depth=2, dim=100, queries_dim=query_creator.output_shape()[-1])
         model.eval()
-        model_input = torch.randn((4, 256, 100))
+        model_input = torch.randn((2, 256, 100))
         model_out = model(model_input, queries=out)
         # Reshape back to correct shape
         model_out = einops.rearrange(
@@ -50,4 +50,4 @@ def test_learnable_query_qpplication(layer_shape):
             h=output_shape[1],
             w=output_shape[2],
         )
-        assert model_out.shape == (4, 6, 803, 32, 32)
+        assert model_out.shape == (2, 6, 227, 16, 16)
